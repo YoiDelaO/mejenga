dc2b13ad308c80c3332682bc987a3b98ede1b869
Kendall Delgado <kendall.delgado.salazar@gmail.com>
2026-06-01 23:49:16 -0600

    Delete Frontend directory
    Error que cometimos

--- Frontend/android/app/src/main/java/com/mejengas/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.mejengas.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- Frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mejengas.app',
-  appName: 'Mejengas',
-  webDir: 'dist'
-};
-
-export default config;
